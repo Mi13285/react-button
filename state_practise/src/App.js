513e460eeda6b190aeb7940b668b98f88f8a4922
Mi13285 <mixkud95@inbox.ru>
2024-08-27 18:46:40 +0300

    first commit

---
+++ state_practise/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RandomNumber from "./components/RandomNumber.js";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomNumber />
+    </div>
+  );
+}
+
+export default App;
